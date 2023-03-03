31183d44b1eec5ff1a091d456fd8455e19211a37
movarnell <michaelvarnell@icloud.com>
2023-03-02 21:01:57 -0600

    first commit

--- script.js
+++ script.js
@@ -1,86 +1,127 @@
-
-let numGuesses = document.getElementById('numOfAttempts')
-
-
-
-
-/* This is a 2 player version of the classic Hangman game. 1 player will choose the 
-word and the other player will guess and play the game. player 1 will also choose how many 
-chances player 2 can take. */
-
-// sets the word used for the guessing portion of the game
-let firstGuess = prompt("What word or Phrase would you like to use?");
-chosenWord = firstGuess.toUpperCase();
-console.log(chosenWord)
-// below -- Used to evaluate against
-let chosenWordArray =[];
+let numGuesses = 6;
+let gamePlayArea = document.getElementById('gamePlayArea');
+let parentContainer = document.getElementById('parentContainer')
+let chosenWord = "";
+let textBox = document.getElementById('textBox')
+let chosenWordArray = [];
 // used to keep up with empty slots/ show correct guesses
 let blankArray = [];
 // keeps up with wrong letters
 let usedWrongLetters = [];
-// lets the user select just how many tries the opponent gets to make. 
+let guess = document.getElementById('textBox').value;
+document.getElementById('letsPlayBtn').addEventListener('click', letsPlay);
 
-// This part fills the arrays with the starting values.
+const textbox = document.getElementById('textBox'); // Replace 'myTextbox' with the ID of your textbox
+
+textbox.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        if (document.getElementById('textBox').value === "") {
+
+        } else {
+            letsPlayBtn.click()
+            console.log('Enter key pressed!');
+        }
+    }
+});
+
+
 function createArray(chosenWord) {
-    for(let i = 0; i < chosenWord.length; i++) {
+    for (let i = 0; i < chosenWord.length; i++) {
         chosenWordArray.push(chosenWord.charAt(i));
-    if (chosenWord.charAt(i) != " ") {
-    blankArray.push("_");
-    } else {blankArray.push(" ")};
+        if (chosenWord.charAt(i) != " ") {
+            blankArray.push("_");
+        } else { blankArray.push(" ") };
+    }
+    document.getElementById('blankArray').innerText = blankArray.join("  ");
+}
+
+function letsPlay() {
+    let chosenWord = document.getElementById('textBox').value;
+    chosenWord = chosenWord.toUpperCase();
+    console.log(chosenWord);
+    createArray(chosenWord);
+    document.getElementById('instructions').innerText = "Type in 1 letter and click Play to guess, your wrong letters will appear here:";
+    document.getElementById('letsPlayBtn').removeEventListener('click', letsPlay);
+    document.getElementById('letsPlayBtn').addEventListener('click', () => { nextGuess() });
+    document.getElementById('textBox').value = "";
+}
+
+function nextGuess() {
+    let guess = document.getElementById('textBox').value.toUpperCase();
+    console.log(guess.length);
+    if (guess.length <= 1) {
+        for (let i = 0; i < chosenWordArray.length; i++) {
+            if (chosenWordArray[i] === guess) {
+                blankArray[i] = guess;
+                document.getElementById('blankArray').innerText = blankArray.join(" ");
+                document.getElementById('textBox').value = "";
+                checkWinLoss();
+
+            }
+        }
+
+        console.log(blankArray);
+        console.log(chosenWordArray);
+
+
+        if (!blankArray.includes(guess) && !usedWrongLetters.includes(guess)) {
+            numGuesses -= 1;
+            usedWrongLetters.push(guess);
+            document.getElementById('instructions').innerText = "Type in 1 letter and click Play to guess, your wrong letters will appear here: " + usedWrongLetters.join(" ");
+            document.getElementById('textOutput').innerText = "You have: " + numGuesses + " wrong guesses left";
+            document.getElementById('blankArray').innerText = blankArray.join(" ");
+            document.getElementById('textBox').value = "";
+
+            checkWinLoss();
+        }
+    } else {
+        document.getElementById('instructions').innerText = "You can't guess more than 1 letter at a time, try again.";
+        document.getElementById('textBox').value = "";
+    }
+
+    let hangPic = document.getElementById('hangImg');
+    switch (numGuesses) {
+        case 0:
+            hangPic.src = "img/hang7.png";
+            break;
+        case 1:
+            hangPic.src = "img/hang6.png";
+            break;
+        case 2:
+            hangPic.src = "img/hang5.png";
+            break;
+        case 3:
+            hangPic.src = "img/hang4.png";
+            break;
+        case 4:
+            hangPic.src = "img/hang3.png";
+            break;
+        case 5:
+            hangPic.src = "img/hang2.png";
+            break;
+        case 6:
+            hangPic.src = "img/hang1.png";
+            break;
     }
 }
 
-// this calls the function to fill the arrays and feeds in the chosenWord as a parameter. 
-createArray(chosenWord);
+function checkWinLoss() {
+    if (blankArray.includes("_") && numGuesses == 0) {
+        console.log(numGuesses);
+        console.log(usedWrongLetters.length)
+        document.getElementById('textOutput').innerText = "You Lose! " + chosenWordArray.join("") + " was the solution.";
+        const audio = new Audio("mixkit-arcade-fast-game-over-233.wav");
+        audio.play();
+        document.getElementById('instructions').innerText = "Click the play button to play again.";
+        document.getElementById('letsPlayBtn').removeEventListener('click', () => { nextGuess() });
+        document.getElementById('letsPlayBtn').addEventListener('click', () => { window.location.reload() });
 
-// evaluates players guess and adds a wrong guess to the usedWrongLetters array. 
-function evaluateGuess(playerGuess) {
-   // this while statment validates that the user has only entered 1 letter. 
-    while (playerGuess.length > 1){
-        playerGuess = prompt("Your guess can only have 1 letter enter it again:");
-        playerGuess = playerGuess.toUpperCase();
     }
-    for (let i = 0; i < chosenWordArray.length; i++) {
-        if(chosenWordArray[i] === playerGuess) {
-        blankArray[i] = playerGuess;
-       
-        } 
-    }    
-        if (!blankArray.includes(playerGuess) && !usedWrongLetters.includes(playerGuess)){
-            usedWrongLetters.push(playerGuess);
-            
-        }
-} 
-
-    
-
-// This is the meat of the game, it runs until the max number of guesses is hit, or until the puzzle is solved.
-// each time it evaluates to see if the player has gotten their guess correct or not. 
-     do {playerGuess = prompt(
-        ` ${blankArray.join(" ")}
-        So far you have used the following letters:
-        ${usedWrongLetters}
-        You have ${numGuesses - usedWrongLetters.length} tries left.
-        What is your next guess?`);
-        playerGuess = playerGuess.toUpperCase();
-        evaluateGuess(playerGuess); 
-        // if the user guess is not correct the below code will let the player know they have lost. 
-        if (usedWrongLetters.length > numGuesses-1) {
-            alert(`You have guessed wrong ${numGuesses} times and did not guess the word correctly.
-            the correct word was ${chosenWord}. You final correct letters were:
-            ${blankArray.join(" ")} and your wrong letters were ${usedWrongLetters}. Sorry play again soon!`)
-        } 
-        playerGuess = playerGuess.toUpperCase();
-    } while (blankArray.includes("_") && usedWrongLetters.length <= numGuesses -1);   
-
-    // displays the win message if the player has won the game. 
     if (!blankArray.includes('_')) {
-    alert(`
-    ${blankArray.join("")} was the solution!
-    You Win!
-    `) 
-} 
+        document.getElementById('textOutput').innerText = "Winner, winner, chicken dinner!";
+        document.getElementById('instructions').innerText = "Click the play button to play again.";
+        document.getElementById('letsPlayBtn').removeEventListener('click', () => { nextGuess() });
+        document.getElementById('letsPlayBtn').addEventListener('click', () => { window.location.reload() });
 
-
-
-
+    }
+}
